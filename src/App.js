bd1a8c5d0ac0bd7a434a4c4a2ed69654f4c5b7c1
HannahHollywood <70626629+HannahHollywood@users.noreply.github.com>
2022-04-09 13:28:38 +0800

    Initial ideas for Whack-a-mole (created Mole function)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// Components
+import Mole from "./components/mole";
+
+// CSS
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button>Start</button>
+      <Mole />
+      <Mole />
+      <Mole />
+      <Mole />
+      <Mole />
+      {/* <img src="" /> */}
     </div>
   );
 }
